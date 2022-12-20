c30c8eb7a3ef7cf330fa768f898423a530dd3f68
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-12-20 18:39:31 -0300

    modifique app

--- client/App.js
+++ client/App.js
@@ -1,24 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from "react-redux"
-import store from "./src/redux/store.js"
+import Navigation from './Navigation';
 
 export default function App() {
   return (
-    <Provider store= {store}>
-    <View style={styles.container}>
-      <Text>Nuestro maravilloso proyecto HENRYconnect</Text>
-      <StatusBar style="auto" />
-    </View>
-    </Provider>
+      <Navigation/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
