cbfec5a865fb3ed5845a3015a0c0ae2ed5d429e3
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-12-20 18:38:13 -0300

    switching branches

--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -1,22 +1,16 @@
 const initialState = {
-    videogames: [],
-    videogamesFilter: [],
-    genre: [],
-    detail: [],
-    status: [],
-    pages: 1
-
+    channels: [],
 }
 
 
 function rootReducer (state = initialState, action){
 switch(action.type){
 
-    case "GET_VIDEOGAMES":  
+    case "ADD_CHANNELS":  
     return  {
         ...state,
-        videogames: action.payload,
-        videogamesFilter: action.payload //cuando trae los v, hace dos copias
+        channels: action.payload,
+        channelsFilter: action.payload
     }
 
 default: return state;
